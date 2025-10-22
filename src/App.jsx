be4064f058f9e9189af1bd91b13166e2b230eabf
JuanC1476 <juancruzmouriat@gmail.com>
2025-10-22 15:53:35 -0300

    Creada la página Comments.jsx en concordancia como está la página MyComments.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,32 @@ import { Routes, Route} from 'react-router-dom';
 
 // Importaciones de Páginas
 import Home from "./pages/Home";
-import LoginPage from "./pages/LoginRegister"; 
+import LoginPage from "./pages/LoginRegister";
 import Favs from "./pages/Favs";
 import Profile from "./pages/Profile";
 import DashboardPage from "./pages/DashboardPage";
 import BookDetailsPage from "./pages/BookDetailsPage";
 import MyComments from "./pages/MyComments";
+import Comments from "./pages/Comments";
 
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} /> 
-      <Route path="/home" element={<Home />} /> 
+      <Route path="/" element={<Comments />} />
+      <Route path="/home" element={<Home />} />
       <Route path="/favoritos" element={<Favs />} />
       <Route path="/perfil" element={<Profile />} />
       <Route path="/mis-comentarios" element={<MyComments />} />
+      <Route path="/comments" element={<Comments />} />
       <Route path="/book/:id" element={<BookDetailsPage />} />
-     
-      
+
+
       {/* Ruta principal de administración */}
       <Route path="/dashboard" element={<DashboardPage />} />
-   
 
-      
+
+
       {/* Ruta 404 debe ir al final */}
       <Route path="*" element={<div>Página no encontrada (404)</div>} />
     </Routes>
