77fe7c2579338132ad05dea3da784bef07de61f6
Lucas Salvatori <lsalvatori.hms@gmail.com>
2025-10-24 19:55:28 -0300

    coneccion con back deploy

--- src/environments/api.js
+++ src/environments/api.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = 'http://localhost:3000';
+// export const API_BASE_URL = 'http://localhost:3000';
 // TODO: Cambiar a la URL de producción cuando se haga el deploy
-// export const API_BASE_URL = 'https://sababook-back.onrender.com';
+export const API_BASE_URL = 'https://sababook-back.onrender.com';
